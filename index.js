d715f9ba8ecfa43306625167d2b1161f82bd022d
eo_01@protonmail.com <eo_01@protonmail.com>
2018-04-11 19:31:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var animal = 'dog'
 function myAnimal() {
   return animal
 }
-
+var animal2 = 'cat'
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  return animal2
 }
 
 function add2(n) {
